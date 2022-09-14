bab764db097eef21d13be44fbf2263763d8dc445
TEMIRLAN <97175881+Temirlan1212@users.noreply.github.com>
2022-09-14 13:37:01 +0300

    test

---
+++ dist/main.4aecb6f2f8e27549f66f.js
@@ -0,0 +1,28 @@
+!(function () {
+  "use strict";
+  var t = {
+      766: function (t, e, n) {
+        n.r(e),
+          (e.default =
+            '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <meta name="viewport" content="width=device-width,initial-scale=1"/> <title>Document</title> </head> <body> asdf </body> </html> ');
+      },
+      344: function (t, e, n) {
+        n.r(e);
+      },
+    },
+    e = {};
+  function n(o) {
+    var i = e[o];
+    if (void 0 !== i) return i.exports;
+    var r = (e[o] = { exports: {} });
+    return t[o](r, r.exports, n), r.exports;
+  }
+  (n.r = function (t) {
+    "undefined" != typeof Symbol &&
+      Symbol.toStringTag &&
+      Object.defineProperty(t, Symbol.toStringTag, { value: "Module" }),
+      Object.defineProperty(t, "__esModule", { value: !0 });
+  }),
+    n(766),
+    n(344);
+})();
