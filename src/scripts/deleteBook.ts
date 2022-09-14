8fa548cb849e7c42b5ef4667c987d5ce923f94a1
TEMIRLAN <97175881+Temirlan1212@users.noreply.github.com>
2022-09-14 13:05:45 +0300

    d

--- src/scripts/deleteBook.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import { API, config, BooksResponse } from "../helpers/interfaces";
-
-export let deleteBook = async (id: string) => {
-  console.log(id);
-  await axios.delete<BooksResponse>(`${API}/books/delete/${id}`, config);
-};
